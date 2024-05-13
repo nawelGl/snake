e0d02761617fc27d7328fa88a5ad9300bcdbf8b1
nawelGl <ghazal.nawel@gmail.com>
2024-05-13 16:19:43 +0200

    Create Case.java

---
+++ Case.java
@@ -0,0 +1,18 @@
+public class Case {
+    //Reprsente une case du tableau de jeu
+    private int x;
+    private int y;
+
+    public Case(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
